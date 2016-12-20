86480277631db499c6e00a222b8f7be5b2f775c9
yichengjie <626659321@qq.com>
2016-12-20 15:59:27 +0800

    s

--- 事件/event2.js
+++ 事件/event2.js
@@ -26,3 +26,18 @@ jQuery.fn.tabs = function (control) {
 
     return this ;
 } ;
+
+
+$("ul#tabs").tabs("#tabContent") ;
+//程序中可以直接更改选项卡的状态，只需要触发观察列表的change.tabs事件即可
+$("#tabs").trigger("change.tabs","users") ;
+//同时，可以将选项卡的动作和窗口的hash做关联，这样就开始使用浏览器的后退按钮了:
+$("#tabs").bind("change.tabs", function (e, tabName) {
+    window.location.hash = tabName ;
+}) ;
+
+$(window).bind('hashchange', function () {
+    //slice去掉字符串的第一个字符#
+    var tabName = window.location.hash.slice(1) ;
+    $("#tabs").trigger('change.tabs',tabName) ;
+}) ;
